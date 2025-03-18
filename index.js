3db2b1675ad95d5d9019b0c0f8d9296e7293682c
Ian <ronesp.85@gmail.com>
2025-03-17 17:00:57 -0700

    finished project. Need to include project images and github link, fix js for mouseover and contrast mode

--- index.js
+++ index.js
@@ -2,15 +2,59 @@
 // service_8l3gs0q
 // imYni-zi_N7uzV2rK
 
+let isModalOpen = false;
+let contrastToggle = false
+const scaleFactor = 1/20
+
+function moveBackground(e){
+  const shapes = document.querySelectorAll(".shape")
+  const x = e.clientX * scaleFactor;
+  const y = e.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; i++) {
+    const isOdd = i % 2 !== 0
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+function toggleContrast() {
+  document.body.classList.add("dark-theme")
+  if (contrastToggle){
+    document.body.classList.add("dark-theme")
+  } else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+function toggleModal() {
+  isModalOpen = !isModalOpen;
+
+  const modal = document.querySelector(".modal");
+
+  if (isModalOpen) {
+    modal.classList.add("modal--open");
+  } else {
+    modal.classList.remove("modal--open");
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const loading = document.querySelector(".modal__overlay--loading");
   const success = document.querySelector(".modal__overlay--success");
+
+  const modalTriggers = document.querySelectorAll(".click");
+
+  modalTriggers.forEach(trigger => {
+    if (!trigger.classList.contains("modal__exit")) {
+      trigger.addEventListener("click", toggleModal);
+    }
+  });
 
   document
     .getElementById("contact__form")
     .addEventListener("submit", function (e) {
       e.preventDefault();
-      console.log("Hello");
 
       loading.classList.add("modal__overlay--visible");
 
@@ -22,8 +66,6 @@ document.addEventListener("DOMContentLoaded", function () {
           "imYni-zi_N7uzV2rK"
         )
         .then(() => {
-          console.log("This works!");
-
           setTimeout(() => {
             loading.classList.remove("modal__overlay--visible");
 
@@ -35,14 +77,9 @@ document.addEventListener("DOMContentLoaded", function () {
           }, 1000);
         })
         .catch((error) => {
-          console.error("Error:", error);
-          alert("Failed to send the message. Please try again.");
-
           setTimeout(() => {
             loading.classList.remove("modal__overlay--visible");
           }, 1000);
         });
     });
 });
-
-
